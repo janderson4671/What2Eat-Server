8d8e29f472d43b0476ba694a151570ff30055789
Jason Anderson <anderson.jason.az@gmail.com>
2020-09-16 18:05:28 -0600

    Add packages and GetPlacesRequest object

---
+++ src/main/kotlin/requests/GetPlacesRequest.kt
@@ -0,0 +1,18 @@
+package requests
+
+class GetPlacesRequest (var latitude: Float, var longitude: Float, var filters: List<String>) {
+
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as GetPlacesRequest
+
+        if (latitude != other.latitude) return false
+        if (longitude != other.longitude) return false
+        if (filters != other.filters) return false
+
+        return true
+    }
+}
